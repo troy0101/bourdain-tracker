5fe0644d3baffbac6f097d2bbe5fb580e0603076
Troy0101 <troy.bloesch@gmail.com>
2026-02-03 14:44:27 -0500

    Add Firebase credentials

--- firebase-config.js
+++ firebase-config.js
@@ -3,12 +3,12 @@
 // Get them from: https://console.firebase.google.com/
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY_HERE",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyB-zjYGD7bUgTv2DBCSR6wHPngf1gQKO_w",
+  authDomain: "bourdain-tracker.firebaseapp.com",
+  projectId: "bourdain-tracker",
+  storageBucket: "bourdain-tracker.firebasestorage.app",
+  messagingSenderId: "556182737579",
+  appId: "1:556182737579:web:bff5369ef140f0dc47ddee"
 };
 
 // Initialize Firebase
